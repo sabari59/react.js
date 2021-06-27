bcf0546c100faafd0f01d1a36641ad25c3a3a97d
sabari59 <84951225+sabari59@users.noreply.github.com>
2021-06-27 00:04:06 -0700

    Add files via upload

---
+++ Info1.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Info1() {
+  return(
+    <div className="note">
+      <h1>HTML and CSS</h1>
+      <p>Basics of HTML and CSS</p>
+    </div>
+
+  );
+  
+}
+
+export default Info1;
